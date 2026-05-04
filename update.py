db380a2a1b9dd2773325461b8e1f79015e626470
GabeSvbr <gabrielveber193@gmail.com>
2026-05-03 21:39:33 -0300

    Add 'bazaar' to installation commands in update.py

--- update.py
+++ update.py
@@ -36,9 +36,9 @@ def ip_info():
 def download_utilitaries():
     comando = '''
     sudo pacman -S --needed yay python xorg-server curl speedtest-cli libreoffice-fresh git python-pip python thunderbird kitty nemo vlc flatpak zip fuse2 &&
-    yay -S --needed shortwave helium-browser-bin &&
+    yay -S --needed shortwave helium-browser-bin bazaar &&
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
-    flatpak install -y flathub io.github.brunofin.Cohesion org.localsend.localsend_app com.brave.Browser app.ytmdesktop.ytmdesktop io.missioncenter.MissionCenter io.github.kolunmi.Bazaar com.rtosta.zapzap io.gitlab.adhami3310.Impression'''
+    flatpak install -y flathub io.github.brunofin.Cohesion org.localsend.localsend_app com.brave.Browser app.ytmdesktop.ytmdesktop io.missioncenter.MissionCenter com.rtosta.zapzap io.gitlab.adhami3310.Impression'''
 
     try:
         subprocess.run(comando, shell=True, check=True)
